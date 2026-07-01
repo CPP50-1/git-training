17a2b1501e57b15eeac934de531a4ea625a211af
nathan-vqbk <nathan.vanquaquebeke@gmail.com>
2026-07-01 10:32:16 +0200

    feat: add greet_julia
    # Conflicts:
    #	src/greet.py
    
    # Conflicts:
    #	src/greet.py

--- src/greet.py
+++ src/greet.py
@@ -13,17 +13,21 @@ def greet_team(team_name: str) -> str:
     """Greet an entire team."""
     return f"Hello, {team_name} team!"
 
+def greet_julia(name: str) -> str:
+    return f"Hey {name}, glad you're here!"
+
 
 def greet_fiona(name: str):
     """Fiona's personal greeting."""
     return f"Hey {name}, glad you're here!"
 
 
+
 def greet_hannah(name: str) -> str:
     """Hannah's personal greeting."""
     return f"Hey {name}, glad you're here!"
 
-    
+
 
 def greet_bob(name: str) -> str:
     """Greet someone (Bob)"""
