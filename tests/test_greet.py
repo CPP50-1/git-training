ba358ef5e329e262fd9e032022d7e6007dcbc789
GSoMan-dev <ayotte.victor@gmail.com>
2026-07-01 10:32:41 +0200

    test: add test for greet_bob

--- tests/test_greet.py
+++ tests/test_greet.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from src.greet import greet_world, greet_team, greet_fiona
+=======
+from src.greet import greet_world, greet_team, greet_bob
+>>>>>>> 2c5f744 (test: add test for greet_bob)
 
 
 def test_greet_world():
@@ -13,3 +17,6 @@ def test_greet_fiona():
     assert "Fiona" in greet_fiona("Fiona")
     result = greet_fiona("Fiona")
     assert isinstance(result, str) 
+
+def test_greet_bob():
+    assert "Bob" in greet_bob("Bob")
