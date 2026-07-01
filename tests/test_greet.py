407dfd61eaef17e2f323f33a86df65483c1d1f04
leroden70 <denis@glewlwyd/com>
2026-07-01 10:10:49 +0200

    test: add test for greet_karl
    # Conflicts:
    #	tests/test_greet.py

--- tests/test_greet.py
+++ tests/test_greet.py
@@ -1,4 +1,4 @@
-from ..src.greet import greet_world, greet_team, greet_julia, greet_fiona, greet_bob, greet_charlie, greet_hannah
+from ..src.greet import greet_world, greet_team, greet_fiona, greet_bob, greet_charlie, greet_hannah
 
 
 def test_greet_world():
@@ -20,13 +20,17 @@ def test_greet_fiona():
     assert "Fiona" in greet_fiona("Fiona")
     result = greet_fiona("Fiona")
     assert isinstance(result, str)
-    assert isinstance(result, str)
 
 
 def test_greet_hannah():
     assert "Hannah" in greet_hannah("Hannah")
-    assert isinstance(result, str)
+    assert isinstance(result, str) 
 
 
 def test_greet_bob():
     assert "Bob" in greet_bob("Bob")
+
+
+def test_greet_karl():
+    """checks whether the name provided to the function is in the return"""
+    assert "Denis" in greet_karl("Denis")
